75f88b0ebbf730da0880544b24f206193d665b38
VIKTORSHOPOV <viktor.shopov13@gmail.com>
2021-12-06 15:31:42 +0200

    new

---
+++ Numbers/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿
+namespace Numbers
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.lblWord = new System.Windows.Forms.Label();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(35, 39);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(210, 20);
+            this.txtInput.TabIndex = 0;
+            // 
+            // lblWord
+            // 
+            this.lblWord.BackColor = System.Drawing.SystemColors.Control;
+            this.lblWord.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblWord.Location = new System.Drawing.Point(35, 78);
+            this.lblWord.Name = "lblWord";
+            this.lblWord.Size = new System.Drawing.Size(210, 39);
+            this.lblWord.TabIndex = 1;
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(35, 130);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(210, 56);
+            this.btnConvert.TabIndex = 2;
+            this.btnConvert.Text = "С думи";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(294, 211);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.lblWord);
+            this.Controls.Add(this.txtInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label lblWord;
+        private System.Windows.Forms.Button btnConvert;
+    }
+}
+
---
+++ Numbers/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Numbers
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            switch (txtInput.Text)
+            {
+                case "0": lblWord.Text = "Нула"; break;
+                case "1": lblWord.Text = "Едно"; break;
+                case "2": lblWord.Text = "Две"; break;
+                case "3": lblWord.Text = "Три"; break;
+                case "4": lblWord.Text = "Четири"; break;
+                case "5": lblWord.Text = "Пет"; break;
+                case "6": lblWord.Text = "Шест"; break;
+                case "7": lblWord.Text = "Седем"; break;
+                case "8": lblWord.Text = "Осем"; break;
+                case "9": lblWord.Text = "Девет"; break;
+                default:
+                    lblWord.Text = "Не знам";
+                    break;
+            }
+        }
+    }
+}
