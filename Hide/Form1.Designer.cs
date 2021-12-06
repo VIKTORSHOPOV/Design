d830de046cddecc3ee9f0a202550ae0a2ac86cb6
VIKTORSHOPOV <viktor.shopov13@gmail.com>
2021-12-06 15:16:35 +0200

    Add project files.

---
+++ Hide/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿
+namespace Hide
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.bShow = new System.Windows.Forms.Button();
+            this.bHide = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(251, 214);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(300, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // bShow
+            // 
+            this.bShow.Location = new System.Drawing.Point(80, 305);
+            this.bShow.Name = "bShow";
+            this.bShow.Size = new System.Drawing.Size(227, 88);
+            this.bShow.TabIndex = 1;
+            this.bShow.Text = "Show";
+            this.bShow.UseVisualStyleBackColor = true;
+            this.bShow.Click += new System.EventHandler(this.bShow_Click);
+            // 
+            // bHide
+            // 
+            this.bHide.Location = new System.Drawing.Point(483, 305);
+            this.bHide.Name = "bHide";
+            this.bHide.Size = new System.Drawing.Size(227, 88);
+            this.bHide.TabIndex = 2;
+            this.bHide.Text = "Hide";
+            this.bHide.UseVisualStyleBackColor = true;
+            this.bHide.Click += new System.EventHandler(this.bHide_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.bHide);
+            this.Controls.Add(this.bShow);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Hide";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button bShow;
+        private System.Windows.Forms.Button bHide;
+    }
+}
+
