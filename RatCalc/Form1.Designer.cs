9660edcf563dfa80bfc5c5619115f6a63d1ed026
VIKTORSHOPOV <viktor.shopov13@gmail.com>
2021-12-09 15:41:43 +0200

    new

---
+++ RatCalc/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿
+namespace RatCalc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFirstNumber = new System.Windows.Forms.TextBox();
+            this.txtSecondNumber = new System.Windows.Forms.TextBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnSub = new System.Windows.Forms.Button();
+            this.btnMult = new System.Windows.Forms.Button();
+            this.btnDev = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtFirstNumber
+            // 
+            this.txtFirstNumber.Location = new System.Drawing.Point(45, 40);
+            this.txtFirstNumber.Name = "txtFirstNumber";
+            this.txtFirstNumber.Size = new System.Drawing.Size(133, 20);
+            this.txtFirstNumber.TabIndex = 0;
+            this.txtFirstNumber.TextChanged += new System.EventHandler(this.txtFirstNumber_TextChanged);
+            // 
+            // txtSecondNumber
+            // 
+            this.txtSecondNumber.Location = new System.Drawing.Point(207, 40);
+            this.txtSecondNumber.Name = "txtSecondNumber";
+            this.txtSecondNumber.Size = new System.Drawing.Size(133, 20);
+            this.txtSecondNumber.TabIndex = 1;
+            // 
+            // lblResult
+            // 
+            this.lblResult.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblResult.Location = new System.Drawing.Point(377, 42);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(89, 18);
+            this.lblResult.TabIndex = 2;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(99, 86);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(30, 27);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "+";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnSub
+            // 
+            this.btnSub.Location = new System.Drawing.Point(135, 86);
+            this.btnSub.Name = "btnSub";
+            this.btnSub.Size = new System.Drawing.Size(30, 27);
+            this.btnSub.TabIndex = 4;
+            this.btnSub.Text = "-";
+            this.btnSub.UseVisualStyleBackColor = true;
+            this.btnSub.Click += new System.EventHandler(this.btnSub_Click);
+            // 
+            // btnMult
+            // 
+            this.btnMult.Location = new System.Drawing.Point(171, 86);
+            this.btnMult.Name = "btnMult";
+            this.btnMult.Size = new System.Drawing.Size(30, 27);
+            this.btnMult.TabIndex = 5;
+            this.btnMult.Text = "*";
+            this.btnMult.UseVisualStyleBackColor = true;
+            this.btnMult.Click += new System.EventHandler(this.btnMult_Click);
+            // 
+            // btnDev
+            // 
+            this.btnDev.Location = new System.Drawing.Point(207, 86);
+            this.btnDev.Name = "btnDev";
+            this.btnDev.Size = new System.Drawing.Size(30, 27);
+            this.btnDev.TabIndex = 6;
+            this.btnDev.Text = "/";
+            this.btnDev.UseVisualStyleBackColor = true;
+            this.btnDev.Click += new System.EventHandler(this.btnDev_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(377, 84);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btnClear.Size = new System.Drawing.Size(89, 30);
+            this.btnClear.TabIndex = 7;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(523, 169);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnDev);
+            this.Controls.Add(this.btnMult);
+            this.Controls.Add(this.btnSub);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.txtSecondNumber);
+            this.Controls.Add(this.txtFirstNumber);
+            this.Name = "Form1";
+            this.Text = "RatCalc";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFirstNumber;
+        private System.Windows.Forms.TextBox txtSecondNumber;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnSub;
+        private System.Windows.Forms.Button btnMult;
+        private System.Windows.Forms.Button btnDev;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ RatCalc/Form1.cs
@@ -0,0 +1,335 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RatCalc
+{
+    public partial class Form1 : Form
+    {
+        static int gcd(int a, int b)
+        {
+            while (a != b)
+            {
+                if (a > b)
+                {
+                    a = a - b;
+                }
+                else
+                {
+                    b = b - a;
+                }
+            }
+            return a;
+        }
+
+        public int isWhole()
+        {
+            int br = 0;
+            List<char> elements = new List<char>();
+            foreach (char el1 in txtFirstNumber.Text)
+            {
+                elements.Add(el1);
+            }
+            foreach (char el2 in txtSecondNumber.Text)
+            {
+                elements.Add(el2);
+            }
+            foreach (char el in elements)
+            {
+                if (el == '/')
+                {
+                    br++;
+                }
+            }
+            if (br == 2)
+            {
+                return 1;
+            }
+            else if (br == 1)
+            {
+                return -1;
+            }
+            else
+            {
+                return 0;
+            }
+
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (isWhole() == 1)
+            {
+                string number1 = txtFirstNumber.Text;
+                string[] numbers1 = number1.Split('/');
+                string number2 = txtSecondNumber.Text;
+                string[] numbers2 = number2.Split('/');
+
+                int c1 = int.Parse(numbers1[0]);
+                int c2 = int.Parse(numbers2[0]);
+                int z1 = int.Parse(numbers1[1]);
+                int z2 = int.Parse(numbers2[1]);
+
+                int min = int.MaxValue;
+                for (int i = 1; i <= z1 * z2; i++)
+                {
+                    if (i % z1 == 0 && i % z2 == 0)
+                    {
+                        if (i < min)
+                        {
+                            min = i;
+                        }
+                    }
+                }
+
+                int result1 = c1 * (min / z1);
+                int result2 = c2 * (min / z2);
+                lblResult.Text = ((result1 + result2) / gcd(result1, result2)).ToString() + '/' + (min / gcd(result1, result2)).ToString();
+            }
+            else if (isWhole() == 0)
+            {
+                lblResult.Text = (int.Parse(txtFirstNumber.Text) + int.Parse(txtSecondNumber.Text)).ToString();
+            }
+            else
+            {
+                if (txtFirstNumber.Text.ToCharArray().Contains('/'))
+                {
+                    txtSecondNumber.Text += "/1";
+                }
+                else
+                {
+                    txtFirstNumber.Text += "/1";
+                }
+
+                string number1 = txtFirstNumber.Text;
+                string[] numbers1 = number1.Split('/');
+                string number2 = txtSecondNumber.Text;
+                string[] numbers2 = number2.Split('/');
+
+                int c1 = int.Parse(numbers1[0]);
+                int c2 = int.Parse(numbers2[0]);
+                int z1 = int.Parse(numbers1[1]);
+                int z2 = int.Parse(numbers2[1]);
+
+                int min = int.MaxValue;
+                for (int i = 1; i <= z1 * z2; i++)
+                {
+                    if (i % z1 == 0 && i % z2 == 0)
+                    {
+                        if (i < min)
+                        {
+                            min = i;
+                        }
+                    }
+                }
+
+                int result1 = c1 * (min / z1);
+                int result2 = c2 * (min / z2);
+                lblResult.Text = ((result1 + result2) / gcd(result1, result2)).ToString() + '/' + (min / gcd(result1, result2)).ToString();
+
+            }
+        }
+
+        private void txtFirstNumber_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtFirstNumber.Clear();
+            txtSecondNumber.Clear();
+            lblResult.Text = "";
+            txtFirstNumber.Focus();
+        }
+
+        private void btnSub_Click(object sender, EventArgs e)
+        {
+            if (isWhole() == 1)
+            {
+                string number1 = txtFirstNumber.Text;
+                string[] numbers1 = number1.Split('/');
+                string number2 = txtSecondNumber.Text;
+                string[] numbers2 = number2.Split('/');
+
+                int c1 = int.Parse(numbers1[0]);
+                int c2 = int.Parse(numbers2[0]);
+                int z1 = int.Parse(numbers1[1]);
+                int z2 = int.Parse(numbers2[1]);
+
+                int min = int.MaxValue;
+                for (int i = 1; i <= z1 * z2; i++)
+                {
+                    if (i % z1 == 0 && i % z2 == 0)
+                    {
+                        if (i < min)
+                        {
+                            min = i;
+                        }
+                    }
+                }
+
+                int result1 = c1 * (min / z1);
+                int result2 = c2 * (min / z2);
+                lblResult.Text = ((result1 - result2) / gcd(result1, result2)).ToString() + '/' + (min / gcd(result1, result2)).ToString();
+            }
+            else if (isWhole() == 0)
+            {
+                lblResult.Text = (int.Parse(txtFirstNumber.Text) - int.Parse(txtSecondNumber.Text)).ToString();
+            }
+            else
+            {
+                if (txtFirstNumber.Text.ToCharArray().Contains('/'))
+                {
+                    txtSecondNumber.Text += "/1";
+                }
+                else
+                {
+                    txtFirstNumber.Text += "/1";
+                }
+
+                string number1 = txtFirstNumber.Text;
+                string[] numbers1 = number1.Split('/');
+                string number2 = txtSecondNumber.Text;
+                string[] numbers2 = number2.Split('/');
+
+                int c1 = int.Parse(numbers1[0]);
+                int c2 = int.Parse(numbers2[0]);
+                int z1 = int.Parse(numbers1[1]);
+                int z2 = int.Parse(numbers2[1]);
+
+                int min = int.MaxValue;
+                for (int i = 1; i <= z1 * z2; i++)
+                {
+                    if (i % z1 == 0 && i % z2 == 0)
+                    {
+                        if (i < min)
+                        {
+                            min = i;
+                        }
+                    }
+                }
+
+                int result1 = c1 * (min / z1);
+                int result2 = c2 * (min / z2);
+                lblResult.Text = ((result1 - result2) / gcd(result1, result2)).ToString() + '/' + (min / gcd(result1, result2)).ToString();
+            }
+
+
+        }
+
+        private void btnMult_Click(object sender, EventArgs e)
+        {
+            if (isWhole() == 1)
+            {
+                string number1 = txtFirstNumber.Text;
+                string[] numbers1 = number1.Split('/');
+                string number2 = txtSecondNumber.Text;
+                string[] numbers2 = number2.Split('/');
+
+                int c1 = int.Parse(numbers1[0]);
+                int c2 = int.Parse(numbers2[0]);
+                int z1 = int.Parse(numbers1[1]);
+                int z2 = int.Parse(numbers2[1]);
+
+
+                int result1 = c1 * c2;
+                int result2 = z1 * z2;
+                lblResult.Text = (result1 / gcd(result1, result2)).ToString() + '/' + (result2 / gcd(result1, result2)).ToString();
+            }
+            else if (isWhole() == 0)
+            {
+                lblResult.Text = (int.Parse(txtFirstNumber.Text) * int.Parse(txtSecondNumber.Text)).ToString();
+            }
+            else
+            {
+                if (txtFirstNumber.Text.ToCharArray().Contains('/'))
+                {
+                    txtSecondNumber.Text += "/1";
+                }
+                else
+                {
+                    txtFirstNumber.Text += "/1";
+                }
+
+                string number1 = txtFirstNumber.Text;
+                string[] numbers1 = number1.Split('/');
+                string number2 = txtSecondNumber.Text;
+                string[] numbers2 = number2.Split('/');
+
+                int c1 = int.Parse(numbers1[0]);
+                int c2 = int.Parse(numbers2[0]);
+                int z1 = int.Parse(numbers1[1]);
+                int z2 = int.Parse(numbers2[1]);
+
+
+                int result1 = c1 * c2;
+                int result2 = z1 * z2;
+                lblResult.Text = (result1 / gcd(result1, result2)).ToString() + '/' + (result2 / gcd(result1, result2)).ToString();
+
+            }
+        }
+
+        private void btnDev_Click(object sender, EventArgs e)
+        {
+            if (isWhole() == 1)
+            {
+                string number1 = txtFirstNumber.Text;
+                string[] numbers1 = number1.Split('/');
+                string number2 = txtSecondNumber.Text;
+                string[] numbers2 = number2.Split('/');
+
+                int c1 = int.Parse(numbers1[0]);
+                int c2 = int.Parse(numbers2[0]);
+                int z1 = int.Parse(numbers1[1]);
+                int z2 = int.Parse(numbers2[1]);
+
+
+                int result1 = c1 * z2;
+                int result2 = z1 * c2;
+                lblResult.Text = (result1 / gcd(result1, result2)).ToString() + '/' + (result2 / gcd(result1, result2)).ToString();
+            }
+            else if (isWhole() == 0)
+            {
+                lblResult.Text = (int.Parse(txtFirstNumber.Text) / (double)int.Parse(txtSecondNumber.Text)).ToString();
+            }
+            else
+            {
+                if (txtFirstNumber.Text.ToCharArray().Contains('/'))
+                {
+                    txtSecondNumber.Text += "/1";
+                }
+                else
+                {
+                    txtFirstNumber.Text += "/1";
+                }
+
+                string number1 = txtFirstNumber.Text;
+                string[] numbers1 = number1.Split('/');
+                string number2 = txtSecondNumber.Text;
+                string[] numbers2 = number2.Split('/');
+
+                int c1 = int.Parse(numbers1[0]);
+                int c2 = int.Parse(numbers2[0]);
+                int z1 = int.Parse(numbers1[1]);
+                int z2 = int.Parse(numbers2[1]);
+
+
+                int result1 = c1 * z2;
+                int result2 = z1 * c2;
+                lblResult.Text = (result1 / gcd(result1, result2)).ToString() + '/' + (result2 / gcd(result1, result2)).ToString();
+
+            }
+        }
+    }
+}
