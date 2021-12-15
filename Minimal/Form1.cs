37fd5f646e44f7b1c4ddea9ca7cd94819f595225
VIKTORSHOPOV <viktor.shopov13@gmail.com>
2021-12-15 13:48:28 +0200

    new

---
+++ Minimal/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Minimal
+{
+    public partial class Form1 : Form
+    {
+        int min = int.MaxValue;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                int number = r.Next(1, 101);
+                listBox1.Items.Add(number);
+                if (number < min)
+                {
+                    min = number;
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = min.ToString();
+        }
+    }
+}
