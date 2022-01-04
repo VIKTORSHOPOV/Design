32a180a21d26f0ee3c13ddb09dd980df3fa8faba
VIKTORSHOPOV <viktor.shopov13@gmail.com>
2022-01-04 17:44:56 +0200

    blank

---
+++ Usporednik/Form1.Designer.cs
@@ -0,0 +1,357 @@
+﻿namespace Usporednik
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.txtC = new System.Windows.Forms.TextBox();
+            this.txtD1 = new System.Windows.Forms.TextBox();
+            this.txtD2 = new System.Windows.Forms.TextBox();
+            this.txtAngle = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.label12 = new System.Windows.Forms.Label();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.lbl4 = new System.Windows.Forms.Label();
+            this.lbl5 = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(45, 12);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(99, 20);
+            this.txtA.TabIndex = 0;
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(45, 47);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(99, 20);
+            this.txtB.TabIndex = 1;
+            // 
+            // txtC
+            // 
+            this.txtC.Location = new System.Drawing.Point(45, 82);
+            this.txtC.Name = "txtC";
+            this.txtC.Size = new System.Drawing.Size(99, 20);
+            this.txtC.TabIndex = 2;
+            // 
+            // txtD1
+            // 
+            this.txtD1.Location = new System.Drawing.Point(219, 12);
+            this.txtD1.Name = "txtD1";
+            this.txtD1.Size = new System.Drawing.Size(99, 20);
+            this.txtD1.TabIndex = 3;
+            // 
+            // txtD2
+            // 
+            this.txtD2.Location = new System.Drawing.Point(219, 47);
+            this.txtD2.Name = "txtD2";
+            this.txtD2.Size = new System.Drawing.Size(99, 20);
+            this.txtD2.TabIndex = 4;
+            // 
+            // txtAngle
+            // 
+            this.txtAngle.Location = new System.Drawing.Point(403, 12);
+            this.txtAngle.Name = "txtAngle";
+            this.txtAngle.Size = new System.Drawing.Size(99, 20);
+            this.txtAngle.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(19, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "A";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(19, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "B";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(19, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "C";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(188, 12);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(25, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "D1";
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(188, 47);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 20);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "D2";
+            // 
+            // label6
+            // 
+            this.label6.Location = new System.Drawing.Point(359, 12);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 20);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Ъгъл";
+            // 
+            // label7
+            // 
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(55, 120);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(483, 50);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Лице на успоредник";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(66, 170);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(219, 49);
+            this.btn1.TabIndex = 13;
+            this.btn1.Text = "a*b*sin";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(68, 239);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(219, 49);
+            this.btn2.TabIndex = 14;
+            this.btn2.Text = "(d1*d2*sin)/2";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(68, 305);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(219, 49);
+            this.btn3.TabIndex = 15;
+            this.btn3.Text = "S tri = Sqrt(p(p-a)(p-b)(p-c))  : p = (a+b+c)/2 : S trap = S tri * 4";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // lbl1
+            // 
+            this.lbl1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl1.Location = new System.Drawing.Point(303, 170);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(180, 49);
+            this.lbl1.TabIndex = 16;
+            // 
+            // lbl2
+            // 
+            this.lbl2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl2.Location = new System.Drawing.Point(303, 239);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(180, 49);
+            this.lbl2.TabIndex = 17;
+            // 
+            // lbl3
+            // 
+            this.lbl3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl3.Location = new System.Drawing.Point(303, 305);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(180, 49);
+            this.lbl3.TabIndex = 18;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(555, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(636, 742);
+            this.richTextBox1.TabIndex = 19;
+            this.richTextBox1.Text = "";
+            // 
+            // label11
+            // 
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(55, 380);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(483, 50);
+            this.label11.TabIndex = 20;
+            this.label11.Text = "Намиране на диагоналите на успоредник с две страни и ъгъл";
+            this.label11.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btn4
+            // 
+            this.btn4.Location = new System.Drawing.Point(68, 444);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(217, 49);
+            this.btn4.TabIndex = 21;
+            this.btn4.Text = "Click";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // label12
+            // 
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(55, 509);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(483, 50);
+            this.label12.TabIndex = 22;
+            this.label12.Text = "Намиране на страните на успоредник с два диагонала и ъгъл";
+            this.label12.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btn5
+            // 
+            this.btn5.Location = new System.Drawing.Point(68, 572);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(217, 49);
+            this.btn5.TabIndex = 23;
+            this.btn5.Text = "Click";
+            this.btn5.UseVisualStyleBackColor = true;
+            // 
+            // lbl4
+            // 
+            this.lbl4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl4.Location = new System.Drawing.Point(303, 444);
+            this.lbl4.Name = "lbl4";
+            this.lbl4.Size = new System.Drawing.Size(180, 49);
+            this.lbl4.TabIndex = 24;
+            // 
+            // lbl5
+            // 
+            this.lbl5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl5.Location = new System.Drawing.Point(303, 572);
+            this.lbl5.Name = "lbl5";
+            this.lbl5.Size = new System.Drawing.Size(180, 49);
+            this.lbl5.TabIndex = 25;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(403, 47);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(99, 32);
+            this.btnClear.TabIndex = 26;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1207, 756);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.lbl5);
+            this.Controls.Add(this.lbl4);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtAngle);
+            this.Controls.Add(this.txtD2);
+            this.Controls.Add(this.txtD1);
+            this.Controls.Add(this.txtC);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.txtA);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.TextBox txtC;
+        private System.Windows.Forms.TextBox txtD1;
+        private System.Windows.Forms.TextBox txtD2;
+        private System.Windows.Forms.TextBox txtAngle;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Label lbl4;
+        private System.Windows.Forms.Label lbl5;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ Usporednik/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Usporednik
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            var radians = Math.PI * int.Parse(txtAngle.Text) / 180.0;
+            var sin = Math.Round(Math.Sin(radians), 2);
+            lbl1.Text = (double.Parse(txtA.Text) * double.Parse(txtB.Text) * sin).ToString();
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            var radians = Math.PI * int.Parse(txtAngle.Text) / 180.0;
+            var sin = Math.Round(Math.Sin(radians), 2);
+            lbl2.Text = ((double.Parse(txtD1.Text) * double.Parse(txtD2.Text) * sin) / 2).ToString();
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            var a = double.Parse(txtA.Text);
+            var b = double.Parse(txtB.Text);
+            var c = double.Parse(txtC.Text);
+            var p = (a + b + c) / 2;
+            var S_tri = Math.Sqrt(p * (p - a) * (p - b) * (p - c))*2;
+            var S_trap = 4 * S_tri;
+            lbl3.Text = S_tri.ToString();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtA.Clear();
+            txtB.Clear();
+            txtC.Clear();
+            txtD1.Clear();
+            txtD2.Clear();
+            txtAngle.Clear();
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            var a = double.Parse(txtA.Text);
+            var b = double.Parse(txtB.Text);
+            var radians = Math.PI * int.Parse(txtAngle.Text) / 180.0;
+            var cos = Math.Round(Math.Cos(radians), 2);
+            var d1_2ndpower = Math.Pow(a, 2) + Math.Pow(b, 2) - 2 * a * b * cos;
+            var d1 = Math.Sqrt(d1_2ndpower);
+            txtD1.Text = d1.ToString();
+            
+            var radians2 = Math.PI * (180 - int.Parse(txtAngle.Text)) / 180.0;
+            var cos2 = Math.Round(Math.Cos(radians2), 2);
+            var d2_2ndpower = Math.Pow(a, 2) + Math.Pow(b, 2) - 2 * a * b * cos2;
+            var d2 = Math.Sqrt(d2_2ndpower);
+            txtD2.Text = d2.ToString();
+
+        }
+    }
+}
