9e81c462a4cad15b17f72f6709bdbc366d1c4fdc
VIKTORSHOPOV <viktor.shopov13@gmail.com>
2022-01-05 11:56:23 +0200

    new

---
+++ Read/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Read
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int pos, next;
+            Students newStud = new Students();
+            StreamReader r = new StreamReader("clasFile.txt");
+            Encoding.GetEncoding("Unicode");
+            string line = r.ReadLine(); line += "";
+            pos = 0; next = line.IndexOf("", pos);
+            Name = line.Substring(pos, next - pos);
+            pos = next + 1; next = line.IndexOf("");
+            newStud.clas = line.Substring(pos, next - pos);
+            pos = next + 1; next = line.IndexOf("");
+            newStud.id = line.Substring(pos, next - pos);
+            pos = next + 1; next = line.IndexOf("", pos);
+            int pos1 = next + 1; next = line.IndexOf("", pos1);
+            newStud.Name = line.Substring(pos1, next - pos);
+            pos = next + 1; next = line.IndexOf("");
+            newStud.Dbel = double.Parse(line.Substring(pos, next - pos));
+            richTextBox1.Text += "С П Р А В К А" + "\n";
+            richTextBox1.Text += "за успеха на " + newStud.Name + "\n";
+            richTextBox1.Text += newStud.clas
+                + "клас,номер" + newStud.id + "/n";
+            richTextBox1.Text += "БЕЛ   -" + newStud.Dbel + "\n";
+            richTextBox1.Text += "Чужд език _" + newStud.Dforeign + "\n";
+            richTextBox1.Text += "Математика _" + newStud.Dmath + "\n";
+            richTextBox1.Text += "Физика _" + newStud.Dfizika + "\n";
+            richTextBox1.Text += "Химия _" + newStud.Dhimiq + "\n";
+            richTextBox1.Text += "Биология _" + newStud.Dbio + "\n";
+            richTextBox1.Text += "Среден успех _" + newStud.Average() + "\n";
+
+        }
+    }
+}
---
+++ Read/Students.cs
@@ -0,0 +1,33 @@
+﻿namespace Read
+{
+    internal class Students
+    {
+        string name;
+        string did;
+        string adress;
+        string klas;
+        double dbel;
+        double dforeign;
+        double dmath;
+        double dfizika;
+        double dhimiq;
+        double dbio;
+        public string id { get { return did; } set { did = value; } }
+        public string Name { get { return name; } set { name = value; } }
+        public string Adress { get { return adress; } set { adress = value; } }
+        public double Dbel { get { return dbel; } set { dbel = value; } }
+        public double Dforeign { get { return dforeign; } set { dforeign = value; } }
+        public double Dmath { get { return dmath; } set { dmath = value; } }
+        public double Dfizika { get { return dfizika; } set { dfizika = value; } }
+        public double Dhimiq { get { return dhimiq; } set { dhimiq = value; } }
+        public double Dbio { get { return dbio; } set { dbio = value; } }
+        public string clas { get { return clas; } set { clas = value; } }
+
+        public double Average()
+        {
+            return Dbel + Dforeign + Dmath + Dhimiq + Dfizika + Dbio / 6;
+        }
+
+
+    }
+}
